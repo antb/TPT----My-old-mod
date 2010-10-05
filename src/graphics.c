ce61ac69784f958c61ce40460b16ac96c3ee0e81
AntB <psychogiant@hotmail.com>
2010-10-05 19:05:37 +0100

    MERGE: Fire improvments

--- src/graphics.c
+++ src/graphics.c
@@ -1765,7 +1765,7 @@ void draw_parts(pixel *vid)
                 }
                 else if(t==PT_PLSM)
                 {
-                    float ttemp = (float)parts[i].life;
+                    float ttemp = (float)((int)(parts[i].life/2));
                     int caddress = restrict_flt(restrict_flt(ttemp, 0.0f, 200.0f)*3, 0.0f, (200.0f*3)-3);
                     uint8 R = plasma_data[caddress];
                     uint8 G = plasma_data[caddress+1];
@@ -1789,10 +1789,13 @@ void draw_parts(pixel *vid)
                     }
                     else
                     {
-                        cr = R;
-                        cg = G;
-                        cb = B;
-                        blendpixel(vid, nx, ny, cr, cg, cb, 192);
+                        cr = parts[i].life * 8;
+                        cg = parts[i].life * 2;
+                        cb = parts[i].life;
+                        if(cr>255) cr = 255;
+                        if(cg>192) cg = 212;
+                        if(cb>128) cb = 192;
+                        blendpixel(vid, nx, ny, cr, cg, cb, 255);
                         blendpixel(vid, nx+1, ny, cr, cg, cb, 96);
                         blendpixel(vid, nx-1, ny, cr, cg, cb, 96);
                         blendpixel(vid, nx, ny+1, cr, cg, cb, 96);
@@ -1801,7 +1804,7 @@ void draw_parts(pixel *vid)
                         blendpixel(vid, nx-1, ny+1, cr, cg, cb, 32);
                         blendpixel(vid, nx+1, ny+1, cr, cg, cb, 32);
                         blendpixel(vid, nx-1, ny-1, cr, cg, cb, 32);
-                    }
+                   }
                 } else if(t==PT_HFLM)
                 {
                     float ttemp = (float)parts[i].life;
