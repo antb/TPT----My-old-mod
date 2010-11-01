5fb452e5262ed0ae91e62e58b7d15ce4b344efed
Philip <philip@philip-linuxlaptop.(none)>
2010-11-02 05:37:27 +0800

    eraser uses shift-alt instead of alt because linux alt-click is normally move window

--- src/interface.c
+++ src/interface.c
@@ -1484,7 +1484,7 @@ void menu_ui_v3(pixel *vid_buf, int i, int *sl, int *sr, int b, int bq, int mx, 
                     drawrect(vid_buf, x+30, y-1, 29, 17, 255, 0, 0, 255);
                     h = n;
                 }
-                if(!bq && mx>=x+32 && mx<x+58 && my>=y && my< y+15&&(sdl_mod & (KMOD_LALT)))
+                if(!bq && mx>=x+32 && mx<x+58 && my>=y && my< y+15&&(sdl_mod & (KMOD_LALT) && sdl_mod & (KMOD_SHIFT)))
                 {
                     drawrect(vid_buf, x+30, y-1, 29, 17, 0, 255, 255, 255);
                     h = n;
@@ -1518,7 +1518,7 @@ void menu_ui_v3(pixel *vid_buf, int i, int *sl, int *sr, int b, int bq, int mx, 
                     drawrect(vid_buf, x+30, y-1, 29, 17, 255, 0, 0, 255);
                     h = n;
                 }
-                if(!bq && mx>=x+32 && mx<x+58 && my>=y && my< y+15&&(sdl_mod & (KMOD_LALT)))
+                if(!bq && mx>=x+32 && mx<x+58 && my>=y && my< y+15&&(sdl_mod & (KMOD_LALT) && sdl_mod & (KMOD_SHIFT)))
                 {
                     drawrect(vid_buf, x+30, y-1, 29, 17, 0, 255, 255, 255);
                     h = n;
@@ -1555,7 +1555,7 @@ void menu_ui_v3(pixel *vid_buf, int i, int *sl, int *sr, int b, int bq, int mx, 
                     drawrect(vid_buf, x+30, y-1, 29, 17, 255, 0, 0, 255);
                     h = n;
                 }
-                if(!bq && mx>=x+32 && mx<x+58 && my>=y && my< y+15&&(sdl_mod & (KMOD_LALT)))
+                if(!bq && mx>=x+32 && mx<x+58 && my>=y && my< y+15&&(sdl_mod & (KMOD_LALT) && sdl_mod & (KMOD_SHIFT)))
                 {
                     drawrect(vid_buf, x+30, y-1, 29, 17, 0, 255, 255, 255);
                     h = n;
@@ -1591,7 +1591,7 @@ void menu_ui_v3(pixel *vid_buf, int i, int *sl, int *sr, int b, int bq, int mx, 
 
     if(b==1&&h!=-1)
     {
-	if(sdl_mod & (KMOD_LALT))
+	if(sdl_mod & (KMOD_LALT) && sdl_mod & (KMOD_SHIFT))
 	{
 		SLALT = h;
 	}
@@ -1601,7 +1601,7 @@ void menu_ui_v3(pixel *vid_buf, int i, int *sl, int *sr, int b, int bq, int mx, 
     }
     if(b==4&&h!=-1)
     {
-        if(sdl_mod & (KMOD_LALT))
+        if(sdl_mod & (KMOD_LALT) && sdl_mod & (KMOD_SHIFT))
 	{
 		SLALT = h;
 	}
